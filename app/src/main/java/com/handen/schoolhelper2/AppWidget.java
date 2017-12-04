1ab3cc2ab6ec4628392ad6b640e94e012e7775e8
Rabochya305 <ivanhanden@gmail.com>
2017-12-04 13:36:15 +0300

    #11 305 startint appwidget

---
+++ app/src/main/java/com/handen/schoolhelper2/AppWidget.java
@@ -0,0 +1,49 @@
+package com.handen.schoolhelper2;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+import android.*;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class AppWidget extends AppWidgetProvider {
+
+    //https://stackoverflow.com/questions/3455123/programmatically-update-widget-from-activity-service-receiver
+    //https://stackoverflow.com/questions/3310264/frequently-updating-widgets-more-frequently-than-what-updateperiodmillis-allows
+    //https://stackoverflow.com/questions/23220757/android-widget-onclick-listener-for-several-buttons
+    //https://stackoverflow.com/questions/14798073/button-click-event-for-android-widget
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        CharSequence widgetText = context.getString(R.string.appwidget_text);
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.app_widget);
+        views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
